ef418de11306dedda11571ff767c8946c83dcf78
Ivan Labiak <71697480+ILabiak@users.noreply.github.com>
2022-07-14 12:39:32 +0300

    controllers for users table in database

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -8,4 +8,59 @@ module.exports = {
         res.status(400).send(error);
       });
   },
+  getById(req, res) {
+    return User.findByPk(req.params.id)
+      .then((user) => {
+        if (!user) {
+          return res.status(404).send({
+            message: "User Not Found",
+          });
+        }
+        return res.status(200).send(user);
+      })
+      .catch((error) => res.status(400).send(error));
+  },
+  add(req, res) {
+    return User.create({
+      username: req.body.username,
+      passhash: req.body.passhash,
+      email: req.body.email || null,
+    })
+      .then((user) => res.status(201).send(user))
+      .catch((error) => res.status(400).send(error));
+  },
+  update(req, res) {
+    return User.findByPk(req.params.id)
+      .then((user) => {
+        if (!user) {
+          return res.status(404).send({
+            message: "User Not Found",
+          });
+        }
+        return user
+          .update({
+            username: req.body.username || user.username,
+            passhash: req.body.passhash || user.passhash,
+            email: req.body.email || user.email,
+          })
+          .then(() => res.status(200).send(user))
+          .catch((error) => res.status(400).send(error));
+      })
+      .catch((error) => res.status(400).send(error));
+  },
+  delete(req, res) {
+    return User.findByPk(req.params.id)
+      .then((user) => {
+        if (!user) {
+          return res.status(400).send({
+            message: "User Not Found",
+          });
+        }
+        return user
+          .destroy()
+          .then(() => res.status(204).send())
+          .catch((error) => res.status(400).send(error));
+      })
+      .catch((error) => res.status(400).send(error));
+  },
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -9,7 +9,13 @@ router.get("/", function (req, res, next) {
 });
 
 /* Users Router */
-router.get("/api/users", UserController.list);
+router.get("/api/users", UserController.getById);
+router.get("/api/users/:id", UserController.getById);
+router.post("/api/users", UserController.add);
+router.put('/api/users/:id', UserController.update);
+router.delete('/api/users/:id', UserController.delete);
+
+
 // router.get('/api/users/:id', classroomController.getById);
 
 module.exports = router;
